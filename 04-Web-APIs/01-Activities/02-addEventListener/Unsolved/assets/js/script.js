f1e87393f13a5b2e2499f51974b9bf5a99727902
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-05-25 18:13:05 -0500

    completed 04.02 class activity

--- 04-Web-APIs/01-Activities/02-addEventListener/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/02-addEventListener/Unsolved/assets/js/script.js
@@ -1,11 +1,29 @@
 // TODO: Create a variable to hold the count
-
+var counter = 0;
 // TODO: Create variables to select the increment button, decrement button and count heading by their respective ids.
-
+var incrBtn = document.querySelector("#increment");
+var decrBtn = document.querySelector("#decrement");
 // TODO: Create a function that displays the current count on the page
+function currCount(event) {
+    var targetEl = event.target;
+    var count = document.querySelector("#count");
+
+    if (targetEl.matches("#increment")) {
+        counter++;
+        count.textContent = counter;
+    }
+    else if (targetEl.matches("#decrement")) {
+        counter--;
+        count.textContent = counter;
+    }
+
+    console.log(counter);
+}
 
 // TODO: Create an addEventListener that will increment the count on click
 // and calls the function to display the current count
-
+decrBtn.addEventListener("click", currCount);
 // TODO: Create an addEventListener that will decrement the count on click
 // and calls the function to display the current count
+incrBtn.addEventListener("click", currCount);
+
