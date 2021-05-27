2d502dbf6044440d14e423ad5f8585fcf08d23cc
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-05-26 19:14:59 -0500

    completed 04.05 activity

--- 04-Web-APIs/01-Activities/05-True-or-False-Game/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/05-True-or-False-Game/Unsolved/assets/js/script.js
@@ -1,11 +1,34 @@
 // TODO: Create an array with five question objects
+var questions = [
+    { q: "The earth is the fourth planet from the sun.", a: "false" },
+    { q: "The planet Venus has no moons.", a: "true" },
+    { q: "Jupiter is composed mostly of iron.", a: "false" },
+    { q: "The sun is a star of average size.", a: "true" },
+    { q: "A lunar eclipse occurs when the sun passes.", a: "false" }
+];
 
 // TODO: Create a variable to keep track of the score
-
+var score = 0;
 // TODO: Iterate over the questions array and display each question in a confirmation box
+function askQuestions() {
+    for (var i = 0; i < questions.length; i++) {
+        var answer = confirm(questions[i]["q"]);
 
-// TODO: Check the user's answer against the correct answer
+        // TODO: Check the user's answer against the correct answer
+        // TODO: Alert the user if they are correct or wrong. Increment the score accordingly
+        if (String(answer) === questions[i]["a"]) {
+            alert("That is correct!");
+            score++;
+        }
+        else {
+            alert("WRONG!");
+        }
+    }
+}
 
-// TODO: Alert the user if they are correct or wrong. Increment the score accordingly
+askQuestions();
 
 // TODO: At the end of the game, alert the user with the final score
+
+alert("Congratulations! You finished with a total score of " + score + "!");
+
