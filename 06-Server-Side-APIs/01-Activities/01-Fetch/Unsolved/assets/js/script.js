f36b5d51b9997a225040e84da819999d448b71f4
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-08 19:50:54 -0500

    update activity 06.01

--- 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
@@ -2,8 +2,8 @@
 // Then log the response in the console
 function getGiphyApi() {
     var apiUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
-    fetch(apiUrl).then(function () {
-        console.log(apiUrl);
+    fetch(apiUrl).then(function (data) {
+        console.log(data);
     })
 }
 
