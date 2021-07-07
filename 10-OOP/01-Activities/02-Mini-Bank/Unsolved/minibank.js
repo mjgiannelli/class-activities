71098b07ee506083c3466accc56e0c3b5d59f5f0
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-06 20:38:16 -0500

    add solution to 10.02

--- 10-OOP/01-Activities/02-Mini-Bank/Unsolved/minibank.js
+++ 10-OOP/01-Activities/02-Mini-Bank/Unsolved/minibank.js
@@ -1,35 +1,49 @@
 // =============================================================
 // PART 1
-
-function MiniBank(balance) {
-  this.balance = balance;
-  this.getBalance = function() {
-    return this.balance;
-  };
-    // Add a `setBalance()` function
-    // YOUR CODE HERE
-    //
-
-    // Add a `deposit()` function
-    // YOUR CODE HERE
-    //
-
-    // Add a `withdraw()` function
-    // YOUR CODE HERE
-    //
-
-    this.printBalance = function() {
-      console.log(`Balance: ${this.getBalance()}`);
+class MiniBank {
+  constructor(balance) {
+    this.balance = balance;
+    this.getBalance = function () {
+      return this.balance;
     };
+  }
+  
+  // Add a `setBalance()` function
+  setBalance(value) {
+    this.balance = value;
+  }
+  //
+  // Add a `deposit()` function
+  deposit(addition) {
+    let newBalance = this.balance + addition;
+    this.setBalance(newBalance);
+    console.log(`Deposited: ${addition}`);
+  }
+  //
+
+  // Add a `withdraw()` function
+  withdraw(subtraction) {
+    let newBalance = this.balance - subtraction;
+    this.setBalance(newBalance);
+    console.log(`Withdrew: ${subtraction}`);
+  }
+  //
+
+  printBalance() {
+    console.log(`Balance: ${this.getBalance()}`);
+  };
 }
 
 // =============================================================
 // PART 2
 
 // Create a new `bank` object
-
+let tdBank = new MiniBank(100);
 // Print the `bank` balance
-
+tdBank.printBalance();
 // Deposit some money and then print the `bank` balance
-
+tdBank.deposit(35);
 // Withdraw some money and then print the `bank` balance
+tdBank.withdraw(60);
+
+tdBank.printBalance();
