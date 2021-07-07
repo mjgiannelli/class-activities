82c616f97fda4bdcd05c6aea52d6ec97c1556b6b
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-06 19:51:53 -0500

    add solution to 10.01

--- 10-OOP/01-Activities/01-Constructor-Function/Unsolved/catsDogs.js
+++ 10-OOP/01-Activities/01-Constructor-Function/Unsolved/catsDogs.js
@@ -1,15 +1,29 @@
 // Create a constructor function called `Animal` that takes in two parameters `raining` and `noise`
 // Include a function called `makeNoise()`
 //
-// YOUR CODE HERE
+class Animal {
+    constructor(raining, noise) {
+        this.raining = raining;
+        this.noise = noise;
+    }
+
+    makeNoise() {
+        if(this.raining) {
+            console.log(this.noise);
+        } 
+    }
+}
 //
 
 // Creates `dog` and `cat` objects with `raining` and `noise` properties
 let dog = new Animal(true, 'Woof!');
 let cat = new Animal(false, 'Meow!');
 
+
 // Calls the `makeNoise()` methods on the `dog` and `cat` objects
 dog.makeNoise();
 cat.makeNoise();
 
-// BONUS CODE HERE
+// // BONUS CODE HERE
+cat.raining = true;
+cat.makeNoise()
