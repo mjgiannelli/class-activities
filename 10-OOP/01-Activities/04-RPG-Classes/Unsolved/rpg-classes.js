e6479918daf2094005caf003f48251c9d9a2d172
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-08 19:54:50 -0500

    add solution to 10.04

--- 10-OOP/01-Activities/04-RPG-Classes/Unsolved/rpg-classes.js
+++ 10-OOP/01-Activities/04-RPG-Classes/Unsolved/rpg-classes.js
@@ -7,3 +7,41 @@
 // Create two new instances of a `Character`, giving them different names, strength, and hitPoints.
 
 // Call a combination of `printStats()`, `attack()`, and `isAlive()` methods to have the two characters "fight" in your console.
+class Character {
+    constructor(name, strength, hitpoints) {
+        this.name = name;
+        this.strength = strength;
+        this.hitpoints = hitpoints;
+    }
+
+    printStats() {
+        console.log("Name: " + this.name)
+        console.log("Strength: " + this.strength)
+        console.log("Hitpoints: " + this.hitpoints)
+    }
+
+    isAlive() {
+        if(this.hitpoints <= 0) {
+            console.log(`${this.name} is dead`);
+        } else {
+            console.log(
+                `${this.name} has ${this.hitpoints} hitpoints remaining`
+            );
+        }
+    }
+
+    attack(enemy) {
+        console.log(`${this.name} has attacked ${enemy.name}!`)
+        enemy.hitpoints = enemy.hitpoints - this.strength;
+    }
+}
+
+let theRock = new Character('The Rock', 10, 75);
+let stoneCold = new Character('Stone Cold', 20, 50);
+
+theRock.printStats();
+stoneCold.printStats();
+
+stoneCold.attack(theRock);
+theRock.printStats();
+theRock.isAlive();
