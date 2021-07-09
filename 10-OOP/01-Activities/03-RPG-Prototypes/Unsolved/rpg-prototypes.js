0a63501c5adac53e4efe81786ca932792ee24841
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-08 19:46:47 -0500

    complete solution 10.03

--- 10-OOP/01-Activities/03-RPG-Prototypes/Unsolved/rpg-prototypes.js
+++ 10-OOP/01-Activities/03-RPG-Prototypes/Unsolved/rpg-prototypes.js
@@ -1,9 +1,36 @@
 // Create a constructor function called `Character` that takes in 3 arguments: `name`, `strength`, `hitpoints`
-// YOUR CODE HERE
+class Character {
+    constructor(name, strength, hitpoints) {
+        this.name = name;
+        this.strength = strength;
+        this.hitpoints = hitpoints;
+    }
+
+    printStats() {
+        console.log("Name: " + this.name)
+        console.log("Strength: " + this.strength)
+        console.log("Hitpoints: " + this.hitpoints)
+    }
+
+    isAlive() {
+        if(this.hitpoints <= 0) {
+            console.log(`${this.name} is dead`);
+        } else {
+            console.log(
+                `${this.name} has ${this.hitpoints} hitpoints remaining`
+            );
+        }
+    }
+
+    attack(enemy) {
+        console.log(`${this.name} has attacked ${enemy.name}!`)
+        enemy.hitpoints = enemy.hitpoints - this.strength;
+    }
+}
 //
 
 // Create a prototype method called `printStats()` which prints all of the stats for a character
-// YOUR CODE HERE
+
 //
 
 // Create a prototype method called `isAlive()` which prints whether or not this character is alive
