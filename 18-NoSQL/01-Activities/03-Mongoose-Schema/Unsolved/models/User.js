d5a0c97aab6a8fc7717320b25a9ba5af14d16e5e
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-02 19:42:43 -0500

    add solution to 18.03

--- 18-NoSQL/01-Activities/03-Mongoose-Schema/Unsolved/models/User.js
+++ 18-NoSQL/01-Activities/03-Mongoose-Schema/Unsolved/models/User.js
@@ -2,7 +2,26 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
   // Add these four attributes to your schema: username, password, email, userCreated
-  // YOUR CODE HERE
+  username: {
+    type: String,
+    required: 'Password is required',
+    trim: true
+  },
+  password: {
+    type: String,
+    trim: true,
+    minLength: 6,
+    required: 'Password is required'
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: /.+\@.+\..+/
+  },
+  userCreated: {
+    type: Date,
+    default: Date.now
+  }
   //
 });
 
