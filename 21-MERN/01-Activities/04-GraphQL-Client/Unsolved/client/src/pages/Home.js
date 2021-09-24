29940386eb59a0261df207db77a5607e83ce894f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-23 20:15:20 -0500

    add solution to 21.04

--- 21-MERN/01-Activities/04-GraphQL-Client/Unsolved/client/src/pages/Home.js
+++ 21-MERN/01-Activities/04-GraphQL-Client/Unsolved/client/src/pages/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import BookList from '../components/BookList';
 // import QUERY_BOOKS
+import { QUERY_BOOKS } from '../utils/queries'
 
 const Home = () => {
   // use useQuery hook to make query request
-  // YOUR CODE HERE
+  const { loading, data } = useQuery(QUERY_BOOKS)
   //
 
   const books = data?.books || [];
